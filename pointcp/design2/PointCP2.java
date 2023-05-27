83cfe365a557ecf879a9387baaa37e6bc658e12d
Jason <jshum086@uottawa.ca>
2023-05-27 13:25:42 -0400

    updates

--- pointcp/design2/PointCP2.java
+++ pointcp/design2/PointCP2.java
@@ -11,5 +11,48 @@ package design2;
  */
 
 public class PointCP2 {
-    
+    private char typeCoord;
+    private double xOrRho;
+    private double yOrTheta;
+
+    public PointCP2(char type, double xOrRho, double yOrTheta){
+        if (type != 'C' && type != 'P'){
+            throw new IllegalArgumentException();
+        }
+        this.xOrRho = xOrRho;
+        this.yOrTheta = yOrTheta;
+        typeCoord = type;
+    }
+
+    public double getX(){
+        if (typeCoord == 'C'){
+            return xOrRho;
+        } else{
+            return (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
+        }
+    }
+
+    public double getY(){
+        if (typeCoord == 'C'){
+            return yOrTheta;
+        } else{
+            return (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
+        }
+    }
+
+    public double getRho(){
+        if (typeCoord == 'P'){
+            return xOrRho;
+        } else{
+            return (Math.sqrt(Math.pow(xOrRho, b:2) + Math.pow(yOrTheta, b:2)));
+        }
+    }
+
+    public double getTheta(){
+        if (typeCoord == 'P'){
+            return yOrTheta;
+        } else{
+            return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
+        }
+    }
 }
